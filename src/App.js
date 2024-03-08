c4ff72b12bdfc006ba2cf8686f0f9b3ce63b8a46
Durdona Ibragimova <durdona.dev@gmail.com>
2024-03-08 00:00:13 -0500

    When button clicked generate random marked cells

--- src/App.js
+++ src/App.js
@@ -4,11 +4,30 @@ import { useState } from "react";
 export default function App() {
     const [difficulty, setDifficulty] = useState(2);
     const [cells, setCells] = useState(Array(4).fill(null));
+    const [markedCells, setMarkedCells] = useState(null);
 
     const handleOnChange = (e) => {
         const { value } = e.target;
         setDifficulty(value);
         setCells(Array(value ** 2).fill(null));
+    };
+
+    const generateRandomCells = (difficulty) => {
+        const cells = [];
+
+        while (cells.length < difficulty) {
+            const randomCell = Math.floor(Math.random() * difficulty ** 2);
+            if (cells.indexOf(randomCell) === -1) {
+                cells.push(randomCell);
+            }
+        }
+
+        let cellsSet = {};
+        for (let ele of cells) {
+            cellsSet[ele] = true;
+        }
+
+        setMarkedCells(cellsSet);
     };
 
     return (
@@ -22,14 +41,21 @@ export default function App() {
                 step="1"
                 onChange={handleOnChange}
             />
-            <button>Start Game</button>
+            <button onClick={generateRandomCells}>Start Game</button>
 
             <div
                 className="grid"
-                style={{ gridTemplateColumns: `repeat(${difficulty}, 1fr` }}
+                style={{ gridTemplateColumns: `repeat(${difficulty}, 1fr)` }}
             >
                 {cells.map((cell, idx) => {
-                    return <div key={idx} className="grid__cell"></div>;
+                    return (
+                        <div
+                            key={idx}
+                            className={
+                                markedCells ? "marked-cell" : "grid__cell"
+                            }
+                        ></div>
+                    );
                 })}
             </div>
         </div>
